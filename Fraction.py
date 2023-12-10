4aa31b6e8fd852d495bbfccb11e54f1009d5eec8
FAIZAAN ALI BAIG <97731707+faizaanalibaig@users.noreply.github.com>
2023-12-11 01:07:21 +0530

    Update Fraction.py

--- Fraction.py
+++ Fraction.py
@@ -14,33 +14,52 @@ class Fraction:
 
   # __add__ is a special method for addition of fractions
   def __add__(self,other):
+    # Addition of fractions: (a/b) + (c/d) = (ad + bc) / bd
     n_num = self.num*other.den + self.den*other.num
     d_num = self.den*other.den
     return '{}/{}'.format(n_num,d_num)
 
   # __sub__ is a special method for subtraction of fractions
   def __sub__(self,other):
+    # Subtraction of fractions: (a/b) - (c/d) = (ad - bc) / bd
     n_num = self.num*other.den - self.den*other.num
     d_num = self.den*other.den
     return '{}/{}'.format(n_num,d_num)
 
   # __mul__ is a special method for multiplication of fractions
   def __mul__(self.other):
+    # Multiplication of fractions: (a/b) * (c/d) = ac / bd
     n_num = self.num*other.num
     d_num = self.den*other.den
     return '{}/{}'.format(n_num,d_num)
 
   # __truediv__ is a special method for division of fractions
   def __truediv__(self.other):
+    # Division of fractions: (a/b) / (c/d) = ad / bc
     n_num = self.num*other.den
     d_num = self.den*other.num
     return '{}/{}'.format(n_num,d_num)
 
-# Constructing an object of this class that takes two arguments which are numerator and denominator
-# let it be X
+# Create instances of the Fraction class that take two arguments which are numerator and denominator
+x = Fraction(4, 5)
+y = Fraction(7, 3)
 
-X = Fraction(4,5)
+# Display the fractions
+print("Fraction X:", x)
+print("Fraction Y:", y)
 
-print(X)
-# when we print X the output should be like -----> 4/5
+# Perform addition
+result_addition = x + y
+print("Addition (X + Y):", result_addition)
 
+# Perform subtraction
+result_subtraction = x - y
+print("Subtraction (X - Y):", result_subtraction)
+
+# Perform multiplication
+result_multiplication = x * y
+print("Multiplication (X * Y):", result_multiplication)
+
+# Perform division
+result_division = x / y
+print("Division (X / Y):", result_division)
